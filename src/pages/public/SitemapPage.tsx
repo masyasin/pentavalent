d3e4ce9f4956342954358b0100c936ec682c85a6
sintechabadi <sintech.ba@gmail.com>
2026-02-02 18:18:32 +0700

    fix: resolve JSX syntax error in SitemapPage

--- src/pages/public/SitemapPage.tsx
+++ src/pages/public/SitemapPage.tsx
@@ -193,6 +193,7 @@ const SitemapPage: React.FC = () => {
                         </button>
                     </motion.div>
                 </div>
+            </div>
             </main>
 
             <Footer onNavigate={() => {}} />
